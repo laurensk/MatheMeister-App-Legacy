f8d59dcc254450230b975e693f47fcfed3b1ad0e
Laurens <laurens@MacBook-Pro-16-von-Laurens.local>
2020-03-26 23:11:56 +0100

    questionanswer object bug fixed

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -15,10 +15,10 @@ class Question {
     this.queId = jsonMap['que_id'];
     this.queLevel = jsonMap['que_level'];
     this.queQuestion = jsonMap['que_question'];
-    this.queAnswers.add(jsonMap['que_correctAnswer']);
-    this.queAnswers.add(jsonMap['que_wrongAnswerOne']);
-    this.queAnswers.add(jsonMap['que_wrongAnswerTwo']);
-    this.queAnswers.add(jsonMap['que_wrongAnswerThree']);
+    this.queAnswers.add(QuestionAnswer(answer: jsonMap['que_correctAnswer'], correctAnswer: true));
+    this.queAnswers.add(QuestionAnswer(answer: jsonMap['que_wrongAnswerOne'], correctAnswer: false));
+    this.queAnswers.add(QuestionAnswer(answer: jsonMap['que_wrongAnswerTwo'], correctAnswer: false));
+    this.queAnswers.add(QuestionAnswer(answer: jsonMap['que_wrongAnswerThree'], correctAnswer: false));
     this.queCreationDate = jsonMap['que_creationDate'];
     this.queCreatorUuid = jsonMap['queCreatorUuid'];
     this.queCreatorFullname = jsonMap['que_creatorFullname'];
